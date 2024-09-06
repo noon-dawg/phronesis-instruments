05c3d92ef93b2908835ee667ee0472314e1ed26a
Alen Mehic <alen.mehic@rebilly.com>
2024-09-06 16:14:19 +0100

    Initial form setup

--- main.js
+++ main.js
@@ -3,50 +3,11 @@
 // 	organizationId: "phronesis-gulliver-s-gallivants",
 // 	websiteId: "www.rebilly.com-",
 // 	apiMode: "sandbox",
-// 	paymentInstruments: {
-// 		address: {
-// 			show: ["all"],
-// 			require: ["organization", "address", "address2", "city", "email", "phoneNumber", "region", "postalCode", "country"],
-// 		},
-// 	},
-// 	items: [
-// 		{
-// 			planId: "monthly-membership",
-// 			quantity: 1,
-// 		},
-// 	],
-// 	addons: [
-// 		{
-// 			planId: "personal-travel-plan",
-// 			quantity: 1,
-// 		},
-// 	],
-// 	bumpOffer: [
-// 		{
-// 			planId: "monthly-membership-plat",
-// 			quantity: 1,
-// 		},
-// 	],
-// 	theme: {
-// 		colorPrimary: "#504CCA", // Brand color
-// 		colorText: "#CEE8F3",
-// 		colorDanger: "#cd5c5c",
-// 		colorBackground: "#021311", // Website background color
-// 		buttonColorText: "#1ABBC7",
-// 		fontFamily: "Trebuchet MS, sans-serif", // Website font family
+// 	money: {
+// 		amount: 100,
+// 		currency: "USD",
 // 	},
 // });
-
-RebillyInstruments.mount({
-	publishableKey: "pk_sandbox_VGoV6xf8qRVq0FvRkz36BzW2nBjThgXVL_gEXgp",
-	organizationId: "phronesis-gulliver-s-gallivants",
-	websiteId: "www.rebilly.com-",
-	apiMode: "sandbox",
-	money: {
-		amount: 100,
-		currency: "USD",
-	},
-});
 // Optional
 RebillyInstruments.on("instrument-ready", (instrument) => {
 	console.info("instrument-ready", instrument);
